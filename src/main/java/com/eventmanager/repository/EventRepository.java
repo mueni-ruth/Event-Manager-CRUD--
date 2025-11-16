99dcfcc4af4aa414d81bfd9cbc600e4c77f6da77
mueni-ruth <ruthmueni054@gmail.com>
2025-11-16 04:57:22 +0300

    second commit

--- src/main/java/com/eventmanager/repository/EventRepository.java
+++ src/main/java/com/eventmanager/repository/EventRepository.java
@@ -1,38 +1,18 @@
 package com.eventmanager.repository;
- 
+
 import com.eventmanager.entity.Event;
-
 import com.eventmanager.entity.User;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.Query;
-
-import org.springframework.data.repository.query.Param;
-
 import org.springframework.stereotype.Repository;
- 
+
 import java.time.LocalDate;
-
 import java.util.List;
- 
+
 @Repository
-
 public interface EventRepository extends JpaRepository<Event, Long> {
-
     List<Event> findByOrganizer(User organizer);
-
+    
     @Query("SELECT e FROM Event e WHERE e.expiryDate >= :currentDate ORDER BY e.uploadDate DESC")
-
-    List<Event> findActiveEvents(@Param("currentDate") LocalDate currentDate);
-
-    List<Event> findByOrganizerAndExpiryDateGreaterThanEqual(User organizer, LocalDate currentDate);
-
-    // Add this method to debug
-
-    @Query("SELECT COUNT(e) FROM Event e")
-
-    long countEvents();
-
+    List<Event> findActiveEvents(LocalDate currentDate);
 }
- 
